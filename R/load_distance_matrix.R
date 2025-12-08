9ca466b637e8ab3c6267131f8707c819962d653a
Scott Claessens <scott.claessens@gmail.com>
2025-12-08 11:32:47 +0000

    Load data

---
+++ R/load_distance_matrix.R
@@ -0,0 +1,20 @@
+# function to load distance matrix
+load_distance_matrix <- function(file, log = FALSE) {
+  # load distances and convert to matrix
+  distance_matrix <- 
+    read_excel(file, na = "") |>
+    dplyr::select(-ISO) |>
+    as.matrix()
+  # set column and row names
+  rownames(distance_matrix) <- colnames(distance_matrix)
+  # log distances?
+  if (log) {
+    distance_matrix <- log(distance_matrix)
+  }
+  # distances between 0 and 1
+  distance_matrix <- distance_matrix / max(distance_matrix)
+  # country has zero distance from itself
+  diag(distance_matrix) <- 0
+  # return distance matrix
+  distance_matrix
+}
--- _targets.R
+++ _targets.R
@@ -1,8 +1,35 @@
 library(targets)
-tar_option_set(packages = c("tidyverse"))
+tar_option_set(packages = c("countrycode", "haven", "readxl", "tidyverse"))
 tar_source()
 
 # pipeline
 list(
-  
+  # load gps data
+  tar_target(data_file, "data/gps/country.dta", format = "file"),
+  tar_target(data, load_gps_data(data_file)),
+  # load geographic distances
+  tar_target(
+    geographic_distance_matrix_file,
+    "data/networks/1F Population Distance.xlsx",
+    format = "file"
+  ),
+  tar_target(
+    geographic_distance_matrix,
+    load_distance_matrix(geographic_distance_matrix_file, log = TRUE)
+  ),
+  # load linguistic distances
+  tar_target(
+    linguistic_distance_matrix_file,
+    "data/networks/2F Country Distance 1pml adj.xlsx",
+    format = "file"
+  ),
+  tar_target(
+    linguistic_distance_matrix,
+    load_distance_matrix(linguistic_distance_matrix_file, log = FALSE)
+  ),
+  # print session info
+  tar_target(
+    sessionInfo,
+    writeLines(capture.output(sessionInfo()), "sessionInfo.txt")
+  )
 )
